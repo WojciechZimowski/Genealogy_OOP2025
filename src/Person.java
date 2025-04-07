868e5254b8e0fd6393387a1bc4f2b8fcad2917df
Wojciech <wenter2005pl@gmail.com>
2025-04-07 15:12:50 +0200

    eene

--- src/Person.java
+++ src/Person.java
@@ -1,7 +1,8 @@
 import java.time.LocalDate;
 import java.util.Set;
 
-public class Person {
+public class Person implements Comparable<Person> {
+
 
 
         private String fname, lname;
@@ -15,15 +16,26 @@ public class Person {
 
         }
         public Person getYoungestChild(){
-            if(children.isEmpty())
-                return null;
-            LocalDate maxDate = LocalDate.now();
+            //jak coś pójdzie nie tak
+//            if(children.isEmpty())
+//                return null;
+            LocalDate youngestChildAge = LocalDate.MIN;
+            Person youngestChild = null;
             for(Person child : children){
-                child.birthDate = maxDate;
+                if(child.birthDate.isAfter(youngestChildAge)){
+                    youngestChildAge = child.birthDate;
+                    youngestChild = child;
 
+                }
             }
+            return youngestChild;
+
         }
 
 
+    @Override
+    public int compareTo(Person o) {
+        this.birthDate.compareTo(o.birthDate);
     }
+}
 
