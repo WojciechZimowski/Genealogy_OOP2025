1239a4a42ce3fabbca5243e0c4b47a185ce1041c
Wojciech <wenter2005pl@gmail.com>
2025-05-04 17:09:05 +0200

    eene

--- src/Main.java
+++ src/Main.java
@@ -11,8 +11,8 @@ public class Main {
 //        System.out.println("END OF PROGRAM");
         List<Person> people = new ArrayList<>();
         Person parent = new Person("Alojzy", "Czeremcha", LocalDate.of(1903,12,7),LocalDate.of(2000,7,12));
-        Person child1 = new Person("Grażyna", "Duda", LocalDate.of(1951,4,7));
-        Person child2 = new Person("Ksawery", "Czeremcha", LocalDate.of(1955,8,12));
+        Person child1 = new Person("Grażyna", "Duda", LocalDate.of(1951,4,7), LocalDate.of(2002,7,12));
+        Person child2 = new Person("Ksawery", "Czeremcha", LocalDate.of(1955,8,12) ,LocalDate.of(2001,7,12));
         Person child3 = new Person("Brajan", "Czeremcha", LocalDate.of(1959,2,18));
         Person child4 = new Person("Dżesika", "Mercedes", LocalDate.of(2000,7,11));
         people.add(parent);
@@ -27,16 +27,18 @@ public class Main {
         parent.adopt(child4);
         //System.out.println("ufjjdjjdjdd");
         //zadanie 5
+        System.out.println("\n Zadanie 5 Latami urodzenia do najmłodszego\n");
         List<Person> sortPeopleByBirthYear = Person.sortPeopleByBirthYear(people);
         for(Person p : sortPeopleByBirthYear){
            System.out.println(p.getFname()+ " "+p.getLname()+ " "+ p.getBirthDate().getYear());
         }
+        System.out.println("\n Zadanie 6 Latami życia w dół\n");
         //zadanie 6
         List<Person> getSortedDeceased = Person.getSortedDeceased(people);
         for(Person p : getSortedDeceased){
             if(p.getDeathDate()!=null) {
                 long lifespan = Period.between(p.getBirthDate(), p.getDeathDate()).getYears();
-                System.out.println(p.getFname() + " " + p.getLname() + " " + lifespan + "lat\n");
+                System.out.println(p.getFname() + " " + p.getLname() + " " + lifespan + "lat");
 
             }
         }
